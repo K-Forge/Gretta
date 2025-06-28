97f580952e5def9f984e02b2a9fa15edd6412399
Lina Bello <bellolina011@gmail.com>
2025-06-28 16:23:48 -0500

    We include the Spring Boot base

---
+++ gretta/src/test/java/com/peluqueria/gretta/gretta/GrettaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peluqueria.gretta.gretta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrettaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
