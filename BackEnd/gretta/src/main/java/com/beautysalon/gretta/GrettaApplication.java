ebe8935b0cbc831c8d1b0aaa37c3ee686bfb36a5
Lina Bello <bellolina011@gmail.com>
2025-11-17 19:40:51 -0500

    Bases del Back-End

---
+++ BackEnd/gretta/src/main/java/com/beautysalon/gretta/GrettaApplication.java
@@ -0,0 +1,13 @@
+package com.beautysalon.gretta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrettaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrettaApplication.class, args);
+	}
+
+}
---
+++ BackEnd/gretta/src/test/java/com/beautysalon/gretta/GrettaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beautysalon.gretta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrettaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
